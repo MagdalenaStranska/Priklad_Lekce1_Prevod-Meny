94efbe8979944a05e91d86cb45748e1b92d50204
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-09-30 23:22:47 +0200

    Pridavam nove soubory

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1.Založte si novou prázdnou stránku s JavaScriptovým programem.
+
+// 2.Dejme tomu, že si jako programátoři vyděláváte 20 euro na hodinu. Uložte tuto hodnotu do proměnné wageInEur.
+
+// 3.Spočítejte, kolik je vaše hodinová mzda v českých korunách, jestliže kurz eura je 24.55 Kč. Výsledek zaokrouhlete na celé koruny a uložte do proměnné :var[wageInCzk].
+
+// 4.Vypište obsah proměnné :var[wageInCzk] do webové stránky tak, aby na stránce byl nadpis h1 s obsahem:
+
+// Mzda v korunách: 532 Kč
